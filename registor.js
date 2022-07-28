53f398d29b031e1548c65b63c79c41b1559141aa
Mukhammadsolikh <tvink16y@gmail.com>
2022-07-28 19:17:31 +0500

    Js hw

---
+++ registor.js
@@ -0,0 +1,37 @@
+
+var text = prompt("Logini kiriting")
+
+
+
+if ( text == 'admin'){
+    var parol = +prompt('parolni kiriting')   
+}
+else if( parol == 12345) {
+    alert('login notogri')
+}
+else{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// | promt сураяпти логинизи йозинг деб 
+// ага логин админ  булса унда парол суралади 
+// агар парол 12345 булса хуш келибсиз 
+// булмасам парол хато 
+// агар админ булмаса кириш ман килинади 
